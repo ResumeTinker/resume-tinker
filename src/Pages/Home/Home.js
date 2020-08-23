6717942d0402b84881dd4c45701aa6483bb77394
MadhanMurali <madhanmurali53@gmail.com>
2020-08-23 21:35:14 +0530

    content complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -23,75 +23,122 @@ const Home = () => {
                 </div>
 
                 <div className="home-content-attraction-equation-div-desktop">
-                    <div className="row ">
-                        <div className="col s12 m12 l12 home-content-attraction-equation-card-panel-outer">
-                            <div className="card-panel home-content-attraction-equation-card-panel home-content-attraction-equation-description-panel">
-                                <h6 className="home-content-attraction-equation-description">
-                                    Get your recruiters' attention with our beautiful resume design and your great career
-                                </h6>
-                                <div className="card-panel">
-                                    <h4 className="home-content-attraction-equation-title center-align">
-                                        Your Career <span>+</span> Our Design <span>=</span> <span>Attractive Resume</span>
-                                    </h4>
-                                </div>
-                            </div>                         
+                    <div className="card-panel home-content-attraction-equation-card-panel">
+                        <h6 className="home-content-attraction-equation-description">
+                            Get your recruiters' attention with our beautiful resume design and your great career
+                        </h6>
+                        <div className="card-panel">
+                            <h4 className="home-content-attraction-equation-title center-align">
+                                Your Career <span>+</span> Our Design <span>=</span> <span>Attractive Resume</span>
+                            </h4>
                         </div>
+                    </div> 
+                </div>
+
+                <div className="row card-panel home-content-card-panel valign-wrapper">
+                    <div className="col s12 m12 l6 card-panel">
+                        <h4 className="home-content-title center-align"><span className="red-text">Redefine</span> Attraction</h4>
+                    </div>
+                    <div className="col s12 m12 l6">
+                        <h6 className="home-content-description">
+                            An Attractive resume should show off skill with eye catching design!
+                        </h6>
                     </div>
                 </div>
 
-                <div className="">
-                    <div className="row">
-                        <div className="col s12 m12 l12">
-                            <div className="row">
-                                
-                            </div>
-                            <div className="card-panel home-content-redefine-attraction-card-panel">
-                                    <div className="col s6 m6 l6">
-                                        <h4 className="card-panel home-content-redefine-attraction-title center-align">Redefine Attraction</h4>
-                                    </div>
-                                </div>
-                                <div className="col s6  m6 l6">
-                                    <h6 className="home-content-redefine-attraction-description">
-                                        An Attractive resume should show off skill with eye catching design!
-                                    </h6>
-                                </div>
-                        </div>
+                <div className="row card-panel home-content-card-panel valign-wrapper">
+                    <div className="col s6 m6 l6">
+                        <h6 className="home-content-description">
+                            Be creative right from creating resume
+                        </h6>
+                    </div>
+                    <div className="col s6 m6 l6 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title center-align">It All <span className="red-text">Begins</span> HERE</h4>
+                    </div>
+                </div>
+
+                <div className="row card-panel home-content-card-panel valign-wrapper">
+                    <div className="col s6 m6 l6 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title home-content-begins-title">Your Resume <span className="red-text"><br/>Stays Yours*</span><br/>FOREVER</h4>
+                    </div>
+                    <div className="col s6 m6 l6">
+                        <h6 className="home-content-description">
+                            Your resume should feel unique through out your career!
+                        </h6>
+                    </div>
+                </div>
+
+                <div className="row card-panel home-content-card-panel">
+                    <div className="col s12 m12 l12 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title center-align">Cleaning Up the Mess</h4>
+                    </div>
+                    <div className="col s12 m12 l12">
+                        <h6 className="home-content-description center-align">Your current social media profile may get your resume rejected! <br/>We will guide you to create a job profile.</h6>
                     </div>
                 </div>
 
             </div>
 
             <div className="hide-on-large-only">
+
                 <div className="home-logo center-align">
                     <img src={Logo} alt={"Logo"} />
                     <h4 className="home-slogan">Sell Your <span className="home-slogan-skill">Skill</span></h4>
                 </div>
-                <div className="row">
+
+                <div className="card-panel home-content-attraction-equation-card-panel home-content-attraction-equation-description-panel">
+                    <h6 className="home-content-attraction-equation-description">
+                        Get your recruiters' attention with our beautiful resume design and your great career
+                    </h6>
+                    <div className="card-panel">
+                        <h4 className="home-content-attraction-equation-title center-align">
+                            Your Career <br/><span>Plus</span><br/> Our Design <br/><span>Equals</span><br/> <span>Attractive Resume</span>
+                        </h4>
+                    </div>
+                </div>
+                
+                <div className="row card-panel home-content-card-panel">
                     <div className="col s12 m12 l6">
-                        <div className="card-panel home-content-attraction-equation-card-panel home-content-attraction-equation-description-panel">
-                            <h6 className="home-content-attraction-equation-description">
-                                Get your recruiters' attention with our beautiful resume design and your great career
-                            </h6>
-                            <div className="card-panel">
-                                <h4 className="home-content-attraction-equation-title center-align">
-                                    Your Career <br/><span>Plus</span><br/> Our Design <br/><span>Equals</span><br/> <span>Attractive Resume</span>
-                                </h4>
-                            </div>
-                        </div>                         
+                        <h6 className="home-content-description">
+                            An Attractive resume should show off skill with eye catching design!
+                        </h6>
+                    </div>
+                    <div className="col s12 m12 l6 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title center-align"><span className="red-text">Redefine</span> Attraction</h4>
                     </div>
                 </div>
-                <div className="">
-                    <div className="row card-panel home-content-redefine-attraction-card-panel">
-                        <div className="col s6 card-panel">
-                            <h4 className="home-content-redefine-attraction-title center-align">Redefine Attraction</h4>
-                        </div>
-                        <div className="col s6">
-                            <h6 className="home-content-redefine-attraction-description">
-                                An Attractive resume should show off skill with eye catching design!
-                            </h6>
-                        </div>
+
+                <div className="row card-panel home-content-card-panel valign-wrapper">
+                    <div className="col s6 m6 l6">
+                        <h6 className="home-content-description">
+                            Be creative right from creating resume
+                        </h6>
+                    </div>
+                    <div className="col s6 m6 l6 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title home-content-begins-title">It All <span className="red-text">Begins</span> HERE</h4>
                     </div>
                 </div>
+
+                <div className="row card-panel home-content-card-panel valign-wrapper">
+                    <div className="col s6 m6 l6 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title home-content-begins-title">Your Resume <span className="red-text"><br/>Stays Yours*</span><br/>FOREVER</h4>
+                    </div>
+                    <div className="col s6 m6 l6">
+                        <h6 className="home-content-description">
+                            Your resume should feel unique through out your career!
+                        </h6>
+                    </div>
+                </div>
+
+                <div className="row card-panel home-content-card-panel">
+                    <div className="col s12 m12 l6 card-panel home-content-title-card-panel">
+                        <h4 className="home-content-title center-align">Cleaning Up the Mess</h4>
+                    </div>
+                    <div className="col s12 m12 l6">
+                        <h6 className="home-content-description center-align">Your current social media profile may get your resume rejected! <br/>We will guide you to create a job profile.</h6>
+                    </div>
+                </div>
+
             </div>
 
         </div>
