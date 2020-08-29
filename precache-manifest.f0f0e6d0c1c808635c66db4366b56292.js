53975aa387b4824628496a7bc9c6064d056fd57f
MadhanMurali <madhanmurali53@gmail.com>
2020-08-29 23:02:57 +0530

    Updates

---
+++ precache-manifest.f0f0e6d0c1c808635c66db4366b56292.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2086fd35c1689bd598a839a3c87a1b73",
+    "url": "/resume-tinker/index.html"
+  },
+  {
+    "revision": "24a400c7213abeb566df",
+    "url": "/resume-tinker/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "628125fde185616dce6e",
+    "url": "/resume-tinker/static/css/main.c6a86dac.chunk.css"
+  },
+  {
+    "revision": "24a400c7213abeb566df",
+    "url": "/resume-tinker/static/js/2.725439ad.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/resume-tinker/static/js/2.725439ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628125fde185616dce6e",
+    "url": "/resume-tinker/static/js/main.e0a34efb.chunk.js"
+  },
+  {
+    "revision": "10800756bc9ce387ab52",
+    "url": "/resume-tinker/static/js/runtime-main.464357c6.js"
+  },
+  {
+    "revision": "6c084270ccdeb72fd9f5a5144cea628f",
+    "url": "/resume-tinker/static/media/Raleway-Light.6c084270.ttf"
+  },
+  {
+    "revision": "2ec8557460d3a2cd7340b16ac84fce32",
+    "url": "/resume-tinker/static/media/Raleway-Medium.2ec85574.ttf"
+  },
+  {
+    "revision": "07ac22f3d71e66a0799703116b533ac5",
+    "url": "/resume-tinker/static/media/Raleway-Thin.07ac22f3.ttf"
+  },
+  {
+    "revision": "d4ac082d906764d8ca8e7b5d0e6e1797",
+    "url": "/resume-tinker/static/media/SimpleStarter.d4ac082d.webp"
+  },
+  {
+    "revision": "68b5b4a02ad107754121a8d594d2c18c",
+    "url": "/resume-tinker/static/media/complete-logo-compressed.68b5b4a0.webp"
+  },
+  {
+    "revision": "9579ca8ce60ff526e5e025f6a7bfe4bd",
+    "url": "/resume-tinker/static/media/home_page_bg_red_person.9579ca8c.webp"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/resume-tinker/precache-manifest.f0f0e6d0c1c808635c66db4366b56292.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume-tinker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
